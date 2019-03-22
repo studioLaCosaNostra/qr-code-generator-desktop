0b10ac2858e0b1d0ec8cb8f9c7dcbfc10534f802
Pawel Laski <zniwus@gmail.com>
2019-03-22 14:25:48 +0100

    update deps

--- install-wine.sh
+++ install-wine.sh
@@ -10,6 +10,6 @@ echo "Build Start"
 git clone --depth 1 git://source.winehq.org/git/wine.git
 cd wine
 sudo apt-get update
-sudo apt-get install build-essential gcc-multilib xorg-dev libx11-dev
+sudo apt-get install build-essential gcc-multilib libx11-dev:i386 libfreetype6-dev:i386
 ./configure
 make
