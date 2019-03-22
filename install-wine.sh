1578e10cd8d17c6283e43c0b566e517369fed612
Pawel Laski <zniwus@gmail.com>
2019-03-22 14:08:38 +0100

    fix make

--- install-wine.sh
+++ install-wine.sh
@@ -10,6 +10,6 @@ echo "Build Start"
 git clone --depth 1 git://source.winehq.org/git/wine.git
 cd wine
 sudo apt-get update
-sudo apt-get install build-essential
-./configure --enable-win64
-./make
+sudo apt-get install build-essential:i386
+./configure
+make
