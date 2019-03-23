ed3421e37079708e102ec31c0c338fd9279c0d40
Pawel Laski <zniwus@gmail.com>
2019-03-23 15:34:01 +0100

    update

---
+++ .travis-update-app-package-version.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_new_version() {
+  git checkout master
+  npm version $(git describe)
+  npm install
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f app/package.json
+  git add -f app/package-lock.json
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG > /dev/null 2>&1
+  git push origin master --quiet
+}
+
+setup_git
+
+commit_new_version
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "A new commit with changed country JSON files exists. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes in country JSON files. Nothing to do"
+fi
