59f68c4a529c6ff600af0a4504ed0d788299106a
Travis CI <travis@travis-ci.org>
2019-03-23 16:59:00 +0100

    add fix travis

--- app/.travis-update-app-package-version.sh
+++ app/.travis-update-app-package-version.sh
@@ -6,7 +6,7 @@ setup_git() {
 }
 
 commit_new_version() {
-  git clone --depth=1 https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG
+  git clone --depth=1 https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG $TRAVIS_REPO_SLUG
   cd $TRAVIS_REPO_SLUG/app
   newVersion=$(git describe --abbrev=0)
   npm version $newVersion
